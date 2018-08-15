d5cf7431d42c750248ae7f794078ebddf57c9d34
Tommer <tommerhauglandgit config --global user.email git config --global user.emailgit config --global user.email tommerhaugland@gmail.comgit config --global user.email tommerhaugland>
2018-08-15 12:45:43 +0200

    Manage table
    Added function for comparing table heads.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,67 +1,36 @@
-/* show lightbox when clicking a thumbnail */
-$('a.thumb').click(function(event){
-  event.preventDefault();
-  var content = $('.modal-body');
-  content.empty();
-    var title = $(this).attr("title");
-    $('.modal-title').html(title);
-    content.html($(this).html());
-    $(".modal-profile").modal({show:true});
-});
-
-
-
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("companyTable");
   switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
+  /*Make a loop that will continue until
+  no switching has been done:*/
   while (switching) {
-    // Start by saying: no switching is done:
+    //start by saying: no switching is done:
     switching = false;
-    rows = table.getElementsByTagName("TR");
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
     for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
+      //start by saying there should be no switching:
       shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TH")[0];
+      y = rows[i + 1].getElementsByTagName("TH")[0];
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      //check if the two rows should switch place:
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
     }
   }
 }
